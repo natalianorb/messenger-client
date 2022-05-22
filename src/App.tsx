653498cc7c70ce024b6014b01106d3ae0ca094ea
Норбоева Наталья Сергеевна <nata.norboeva@gmail.com>
2022-05-22 20:11:53 +0900

    basics of login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import logo from './logo.svg'
-import './App.css'
+import Container from '@mui/material/Container'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import SignIn from './pages/sign-in/SignIn'
+
+const theme = createTheme()
 
 function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main">
+      <CssBaseline />
+      <SignIn />
+    </Container>
+    </ThemeProvider>
   )
 }
 
