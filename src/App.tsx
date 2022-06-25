1e5a2400458b30bb5408fd28e71eb060ca9753b1
nata.norboeva <nata.norboeva@gmail.com>
2022-06-25 19:24:26 +0900

    lint changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import Container from '@mui/material/Container'
-import CssBaseline from '@mui/material/CssBaseline'
-import { ThemeProvider, createTheme } from '@mui/material/styles'
-import SignIn from './pages/SignIn'
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import SignIn from './pages/SignIn';
 
-const theme = createTheme()
+const theme = createTheme();
 
-function App () {
+function App() {
   return (
     <ThemeProvider theme={theme}>
       <Container component="main">
-      <CssBaseline />
-      <SignIn />
-    </Container>
+        <CssBaseline />
+        <SignIn />
+      </Container>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+});
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-)
+  document.getElementById('root') as HTMLElement,
+);
 root.render(
-  <ApolloProvider client={ client }>
+  <ApolloProvider client={client}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </ApolloProvider>
-)
+  </ApolloProvider>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
